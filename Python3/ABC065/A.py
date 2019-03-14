a4237251f167b65ccce9fa07957a9ad5b37df0da
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-03-15 05:02:39 +0900

    Create A.py

---
+++ Python3/ABC065/A.py
@@ -0,0 +1,10 @@
+# URL: https://atcoder.jp/contests/abc065/tasks/abc065_a
+
+X, A, B = map(int,input().split())
+
+if(B-A <= 0):
+  print("delicious")
+elif(B-A <= X):
+  print("safe")
+else:
+  print("dangerous")
