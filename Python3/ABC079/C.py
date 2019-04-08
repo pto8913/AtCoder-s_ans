b2e3b78e6bac401671a04da16f9981a8adcc1314
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-04-08 12:31:40 +0900

    Update C.py

--- Python3/ABC079/C.py
+++ Python3/ABC079/C.py
@@ -11,3 +11,24 @@ for op1 in ops:
       if(eval(x) == 7):
         print(x+"=7")
         exit()
+
+        
+# bit
+S = input()
+
+for i in range(1 << 3):
+  tmp = int(S[0])
+  op = []
+  for j in range(3):
+    if ((i >> j) & 1) == 1:
+      op += ["+"]
+      tmp += int(S[j+1])
+    else:
+      op += ["-"]
+      tmp -= int(S[j+1])
+  if tmp == 7:
+    res = ""
+    for j in range(3):
+      res += S[j]+op[j]
+    print(res+S[3]+"=7")
+    break
