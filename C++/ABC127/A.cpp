5987ed20103b6a92afbc7792865bf8a6c37fc3f8
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-24 08:09:24 +0900

    Create A.cpp

---
+++ C++/ABC127/A.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int A, B;
+  cin >> A >> B;
+  if (A > 12) cout << B << endl;
+  else if (A < 6) cout << 0 << endl;
+  else cout << B/2 << endl;
+}
