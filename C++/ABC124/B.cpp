179791aeaaae83ee5669005c9b91dcf3a922a373
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-29 17:28:27 +0900

    Update B.cpp

--- C++/ABC124/B.cpp
+++ C++/ABC124/B.cpp
@@ -20,3 +20,31 @@ int main() {
 	}
 	cout << ans << endl;	
 }
+/*
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for(int i=0; i<n; ++i)
+
+int main(){
+  int N, ans = 1;
+  cin >> N;
+  vector<int> H(N);
+  rep(i, N) cin >> H[i];
+
+  for (int i = 1; i < N; ++i){
+    int cnt = 0;
+    rep(j, i+1){
+      if (H[j] <= H[i]){
+        cnt++;
+      }
+    }
+    if (cnt == i+1){
+      ans++;
+    }
+  }
+  cout << ans << endl;
+}
+*/
