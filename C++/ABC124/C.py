adf2ab61ad0f5a7c9a15bf7dd50af39931fe6890
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-29 17:27:58 +0900

    Update C.py

--- C++/ABC124/C.py
+++ C++/ABC124/C.py
@@ -12,3 +12,32 @@ int main() {
 	}
 	cout << min(ans1, ans2) << endl;
 }
+
+/*
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define rep(i, n) for(int i=0; i<n; ++i)
+
+int main(){
+  string S;
+  cin >> S;
+  int n = S.size();
+  int A = 0, B = 0;
+  rep(i, n){
+    char c;
+    if (i%2 == 0) c = '0';
+    else c = '1';
+    if (S[i] != c) ++A;
+  }
+  rep(i, n){
+    char c;
+    if (i%2 == 1) c = '0';
+    else c = '1';
+    if (S[i] != c) ++B;
+  }
+  cout << min(A, B) << endl;
+}
+*/
