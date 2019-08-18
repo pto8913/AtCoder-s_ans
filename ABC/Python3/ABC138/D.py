d501c75931070b54717515abd1366fe5b23e92a4
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-08-19 00:56:05 +0900

    Update D.py

--- ABC/Python3/ABC138/D.py
+++ ABC/Python3/ABC138/D.py
@@ -1,5 +1,48 @@
 import sys
 
+sys.setrecursionlimit(10**6)
+
+stdin = sys.stdin
+na = lambda: map(int, stdin.readline().split())
+
+g = []
+counter = []
+ans = []
+
+def dfs(cur, par, val):
+  global counter, ans, g
+  val += counter[cur]
+  ans[cur] = val
+  for e in g[cur]:
+    if e == par:
+      continue
+    dfs(e, cur, val)
+
+def main():
+  global g, ans, counter
+  N, Q = na()
+  
+  g = [[] for _ in range(N)]
+  for _ in range(N - 1):
+    a, b = na()
+    g[a-1] += [b-1]
+    g[b-1] += [a-1]
+
+  counter = [0] * N
+  for _ in range(Q):
+    p, x = na()
+    counter[p-1] += x
+
+  ans = [0] * N
+  dfs(0, -1, 0)
+  print(*ans)
+
+if __name__ == "__main__":
+  main()
+
+""" 嘘解法
+import sys
+
 stdin = sys.stdin
 na = lambda: map(int, stdin.readline().split())
 
@@ -22,3 +65,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+"""
